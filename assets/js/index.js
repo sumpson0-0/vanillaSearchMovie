214c044057853b06120566c3cdcbb5e26dabb425
sumpson0-0 <sumpson00@gmail.com>
2020-07-30 22:42:47 +0900

    focus 이벤트 생성

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,19 @@
 const form = document.querySelector('.search-bar__form');
 const input = document.querySelector('.search-bar__input');
 
-const handleSubmit = async () => {
-	event.preventDefault();
-	const search = await input.value;
-	getMovies(search);
+let recentSearchTerms = [];
+let searchId = 0;
+
+const paintRecentSearch = () => {};
+
+const handleFocus = () => {
+	const loadRecentSearch = JSON.parse(localStorage.getItem('movie'));
+	if (loadRecentSearch !== null) {
+		paintRecentSearch();
+	}
 };
 
 form.addEventListener('submit', handleSubmit);
+input.addEventListener('focus', handleFocus);
 
 // response false인 경우 error가 alert나 이런거로 화면에 뜨게하자
